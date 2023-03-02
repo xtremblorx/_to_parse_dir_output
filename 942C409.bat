03b198743833b73f7e949e8b58316b724d89b521
xtremblorx <117502862+xtremblorx@users.noreply.github.com>
2023-03-02 08:41:35 -0800

    Update 942C409.bat

--- 942C409.bat
+++ 942C409.bat
@@ -36,5 +36,5 @@ set /a counter=0
 for /f "skip=4 delims=" %%a in ('dir') do (if !counter! GEQ !total! (goto END)) & set /a counter+=1 &  for /f "tokens=1,2,3,4 delims= " %%b in ("%%a") do echo. & (set filename=%%a& set filename=!filename:~%INDEX%,250!)&echo !filename!
 :END
 PAUSE
-REM Variables in Use= counter,rootfilename,currentdir,Error_code1
+REM Variables in Use= counter,rootfilename,currentdir,Error_code1,total,filename,index
 REM labels in use: 
